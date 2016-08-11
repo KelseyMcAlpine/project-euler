a5852a4c07c20582599d8f518417c44fbbdc9615
Kelsey McAlpine <kmcalpine023@gmail.com>
2016-08-10 20:46:00 -0500

    Update problem1.rb

--- SOLUTIONS/problem1.rb
+++ SOLUTIONS/problem1.rb
@@ -1,2 +1,14 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 # The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+# Find all multiples of 3 <= 1000 and store in array: mult_3 
+
+# Find all multiples of 5 <= 1000 and store in array: mult_5
+
+# Find all multiples of 15 <= 1000 and store in array: mult_15
+
+# Calculate the sum of mult_3 + mult_5
+
+# Calculate the sum of mult_15
+
+# Subtract 
